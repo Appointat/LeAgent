7bff712217aa740cd32a86abcccdbc31429099ce
Zikang CHEN <appointat@shu.edu.cn>
2023-03-22 23:39:14 +0100

    Build the connection of API on Colab

--- Chat-with-Document-s-using-ChatGPT-API-and-Text-Embedding.py
+++ Chat-with-Document-s-using-ChatGPT-API-and-Text-Embedding.py
@@ -1,10 +1,30 @@
+# -*- coding: utf-8 -*-
+"""Chat-with-Document-s-using-ChatGPT-API-and-Text-Embedding.ipynb
+
+This Jupyter notebook demonstrates how to use the ChatGPT API and text embedding to chat with documents. 
+The notebook begins by installing the necessary packages (openai, chromadb, langchain, and tiktoken) and 
+importing required Python packages. The version of Python being used is printed and Google Drive is 
+mounted on Colab for saving data. The OpenAI API key is set and Chroma is configured.
+
+The notebook includes a function to retrieve text from a Gutenberg URL and uses it to load Romeo and 
+Juliet from Project Gutenberg. The text is then split into chunks of 1000 tokens with 0 token overlap. 
+OpenAI embeddings are generated and LangChain QA is configured with OpenAI as the LLM, using a temperature 
+of 0 and the gpt-3.5-turbo model. """
+
+# Install necessary packages
+#!pip install openai
+#!pip install chromadb
+#!pip install langchain
+#!pip install tiktoken
+
+# Import required Python packages
 import os
 import platform
-
 import openai
 import chromadb
 import langchain
 
+# Import specific modules from langchain
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
 from langchain.text_splitter import TokenTextSplitter
@@ -12,65 +32,47 @@ from langchain.llms import OpenAI
 from langchain.chains import ChatVectorDBChain
 from langchain.document_loaders import GutenbergLoader
 
-# from google.colab import drive
-# drive.mount('/content/drive')
+# Print the version of Python being used
+print("Python: ", platform.python_version())
 
-# Set up the OpenAI API key
-os.environ["OPENAI_API_KEY"] = "your openai api key"
+# Mount Google Drive on Colab for saving data
+from google.colab import drive
 
-# Set up Chroma to use a persistent directory on disk
+drive.mount("/content/drive")
+
+# Set the OpenAI API key
+os.environ["OPENAI_API_KEY"] = "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"
+
+# Configure Chroma
 persist_directory = "/content/drive/My Drive/Colab Notebooks/chroma/romeo"
 
 
-# Define a function to load the data from Project Gutenberg
+# Function to retrieve text from a Gutenberg URL
 def get_gutenberg(url):
     loader = GutenbergLoader(url)
     data = loader.load()
     return data
 
 
-# Load the data from Project Gutenberg and split it into chunks using the TokenTextSplitter class:
+# Load Romeo and Juliet from Project Gutenberg
 romeoandjuliet_data = get_gutenberg(
     "https://www.gutenberg.org/cache/epub/1513/pg1513.txt"
 )
 
+# Split the text into chunks of 1000 tokens with 0 token overlap
 text_splitter = TokenTextSplitter(chunk_size=1000, chunk_overlap=0)
 romeoandjuliet_doc = text_splitter.split_documents(romeoandjuliet_data)
 
-# Convert the document to vector embedding and store it in a vector search engine using Chroma
+# Generate OpenAI embeddings
 embeddings = OpenAIEmbeddings()
 vectordb = Chroma.from_documents(
     romeoandjuliet_doc, embeddings, persist_directory=persist_directory
 )
 vectordb.persist()
 
+# Configure LangChain QA with OpenAI as the LLM, using a temperature of 0, and the gpt-3.5-turbo model
 romeoandjuliet_qa = ChatVectorDBChain.from_llm(
     OpenAI(temperature=0, model_name="gpt-3.5-turbo"),
     vectordb,
     return_source_documents=True,
 )
-
-
-# Define a function to get a response from ChatGPT
-def chat_response(prompt):
-    openai.api_key = os.getenv("OPENAI_API_KEY")
-    llms = OpenAI(engine="text-davinci-002", temperature=0.7)
-    chain = ChatVectorDBChain(
-        llms=llms,
-        vectorstore=vectordb,
-        top_k=1,
-        similarity_threshold=0.5,
-        maximum_iterations=20,
-    )
-    response = chain(prompt)
-    return response
-
-
-if __name__ == "__main__":
-    print("Python: ", platform.python_version())
-    # Use the chat_response function to get a response from ChatGPT
-    prompt = "What is Romeo and Juliet about?"
-
-    response = chat_response(prompt)
-
-    print(response)
