355b8f4250fc84b2797aa2ecb7b7a7028882041f
Zikang CHEN <appointat@shu.edu.cn>
2023-03-22 01:03:45 +0100

    Update the dependency

---
+++ Chat-with-Document-s-using-ChatGPT-API-and-Text-Embedding.py
@@ -0,0 +1,17 @@
+import os
+import platform
+
+import openai
+#import chromadb
+import langchain
+
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import Chroma
+from langchain.text_splitter import TokenTextSplitter
+from langchain.llms import OpenAI
+from langchain.chains import ChatVectorDBChain
+from langchain.document_loaders import GutenbergLoader
+
+
+if __name__ == "__main__":
+    print('Python: ', platform.python_version())
