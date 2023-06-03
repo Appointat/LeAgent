fe99234c600567afa644ec43edd9d7e5d0e2641d
Zikang CHEN <appointat@shu.edu.cn>
2023-06-03 15:05:47 +0200

    Add try-catch for multiple processes

--- TraceTalk/handle_multiprocessing.py
+++ TraceTalk/handle_multiprocessing.py
@@ -1,12 +1,16 @@
 # Define the process_request function.
 def process_request(params):
-	chatbot_agent, context, chat_history, summaries, query, link = params
-	chatbot_agent.prompt_chatbot()
-	chain = chatbot_agent.prompt_chatbot()
-	answer = chain.predict(
-		context=context,
-		chat_history=chat_history,
-		summaries=summaries,
-		qury=query,
-	)
-	return answer, link
+    chatbot_agent, context, chat_history, summaries, query, link = params
+    try:
+        chain = chatbot_agent.prompt_chatbot()
+        answer = chain.predict(
+            context=context,
+            chat_history=chat_history,
+            summaries=summaries,
+            qury=query,
+        )
+    finally:
+        # Release resources here, for example:
+        # chatbot_agent.close()
+        pass
+    return answer, link
