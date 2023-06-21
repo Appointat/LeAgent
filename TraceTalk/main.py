8b32263679e97600b5fa08faceb5165bf4d14ddc
Zikang CHEN <appointat@shu.edu.cn>
2023-06-21 23:57:38 +0200

    Update api

--- TraceTalk/main.py
+++ TraceTalk/main.py
@@ -10,13 +10,6 @@ from chatbot_agent import ChatbotAgent
 
 
 def main():
-    # Get the messages (history of conversation) from the command line.
-	messages = []
-	mode = "LOCAL" if len(sys.argv) < 2 else "SERVICE"
-	if mode == "SERVICE":
-		with open('TaceTalk/messages.json', 'r') as file:
-			messages = json.load(file)
-
 	# Initialize the ChatbotAgent.
 	load_dotenv()
 	openai_api_key = os.getenv('OPENAI_API_KEY')
@@ -26,36 +19,16 @@ def main():
 	chatbot_agent = ChatbotAgent(openai_api_key=openai_api_key, messages=messages)
 
 	# Start the conversation.
-	if mode == "LOCAL":
-		print("\n\n****Chatbot Agent Initialized****")
-		while chatbot_agent.count <= 10:
-			print("[{}]".format(chatbot_agent.count))
-			query = input("Query   : ")
-			answer_list = []
-			link_list = []
-			# query it using content vector.
-			query_results = chatbot_agent.search_context_qdrant(chatbot_agent.convert_chat_history_to_string()+"\nuser: "+query, 'Articles', top_k=4)
-			requests = [(chatbot_agent, article.payload["content"], chatbot_agent.convert_chat_history_to_string(), "", query, article.payload["link"]) for article in query_results]
-	
-			# Use a Pool to manage the processes.
-			with ThreadPoolExecutor(max_workers=len(query_results)) as executor:
-				results = list(executor.map(process_request, requests))
-
-			# Results is a list of tuples of the form (answer, link).
-			answer_list, link_list = zip(*results)
-
-			combine_answer = chatbot_agent.prompt_combine_chain(query=query, answer_list=answer_list, link_list=link_list)
-			print(f'Query : {query}\n')
-			print(f'Answer: {combine_answer}\n')
-			chatbot_agent.update_chat_history(query, combine_answer)
-	else:
-		query = messages[-1]["text"]
+	print("\n\n****Chatbot Agent Initialized****")
+	while chatbot_agent.count <= 10:
+		print("[{}]".format(chatbot_agent.count))
+		query = input("Query   : ")
 		answer_list = []
 		link_list = []
 		# query it using content vector.
-		query_results = chatbot_agent.search_context_qdrant(chatbot_agent.convert_chat_history_to_string(), 'Articles', top_k=4)
+		query_results = chatbot_agent.search_context_qdrant(chatbot_agent.convert_chat_history_to_string()+"\nuser: "+query, 'Articles', top_k=4)
 		requests = [(chatbot_agent, article.payload["content"], chatbot_agent.convert_chat_history_to_string(), "", query, article.payload["link"]) for article in query_results]
-	
+
 		# Use a Pool to manage the processes.
 		with ThreadPoolExecutor(max_workers=len(query_results)) as executor:
 			results = list(executor.map(process_request, requests))
@@ -64,11 +37,9 @@ def main():
 		answer_list, link_list = zip(*results)
 
 		combine_answer = chatbot_agent.prompt_combine_chain(query=query, answer_list=answer_list, link_list=link_list)
-
-		print(combine_answer)
-		# chatbot_agent.update_chat_history(query, combine_answer)
-
-
+		print(f'Query : {query}\n')
+		print(f'Answer: {combine_answer}\n')
+		chatbot_agent.update_chat_history(query, combine_answer)
 
 if __name__ == "__main__":
 	main()
