f7f60826157ceca496b6ae872e0f0a151e6918de
Zikang CHEN <appointat@shu.edu.cn>
2023-06-25 14:55:39 +0200

    Update resources

--- TraceTalk/update_collection.py
+++ TraceTalk/update_collection.py
@@ -1,6 +1,8 @@
-from prep_data import prep_data
+from prep_data import prep_book_data, update_collection_to_database
 
 
 
 if __name__ == "__main__":
-   prep_data()
+   scv_file_path = r'TraceTalk\vector-db-persist-directory\book dada\book data.csv'
+   prep_book_data(scv_file_path)
+   update_collection_to_database(scv_file_path)
