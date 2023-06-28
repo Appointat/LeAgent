ecab59e272e12cad2f63463b32f80d4c38d9e55f
Zikang CHEN <appointat@shu.edu.cn>
2023-06-28 23:36:37 +0200

    Refuse to answer

--- TraceTalk/handle_multiprocessing.py
+++ TraceTalk/handle_multiprocessing.py
@@ -1,7 +1,9 @@
 # Define the process_request function.
 def process_request(params):
-    chatbot_agent, context, chat_history, summaries, query, link = params
-    convert_link = link.replace('_sources', '').replace('.md', '.html')
+    chatbot_agent, context, chat_history, summaries, query, link, score = params
+    convert_link = link.replace('_sources', '').replace('.md', '.html') if score > 0.715 else ""
+    reject_context = "sorry, the question is not associate with the context. The chatbot should refuse to answer."
+    
     try:
         chain = chatbot_agent.prompt_chatbot()
         answer = chain.predict(
@@ -9,7 +11,7 @@ def process_request(params):
             chat_history=chat_history,
             summaries=summaries,
             query=query,
-        )
+        ) if score > 0.715 else reject_context
     finally:
         # Release resources here, for example:
         # chatbot_agent.close()
