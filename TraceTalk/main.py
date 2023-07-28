dd755760f01f45edf18f886fdf0ca24a444460c8
Zikang CHEN <appointat@shu.edu.cn>
2023-07-28 23:54:36 +0200

    Update the main function

--- TraceTalk/main.py
+++ TraceTalk/main.py
@@ -34,141 +34,70 @@ def main(message="", messages=[""]):
     )
 
     # Start the conversation.
-    if not message and not use_REST_API:
-        print("\n\n****Chatbot Agent Initialized****")
-        while chatbot_agent.count <= 10:
-            print("[{}]".format(chatbot_agent.count))
-            query = input("Query   : ")
-            answer_list = []
-            link_list = []
-            # Query it using content vector.
-            query_results = chatbot_agent.search_context_qdrant(
-                chatbot_agent.convert_chat_history_to_string(user_only=True)
-                + "\nuser: "
-                + query,
-                "Articles",
-                top_k=4,
-            )
+    query = messages[-1]
+    answer_list = []
+    link_list = []
+    # query it using content vector.
+    query_results = chatbot_agent.search_context_qdrant(
+        chatbot_agent.convert_chat_history_to_string(
+            new_query=query, user_only=True
+        ),
+        "Articles",
+        top_k=4,
+    )
 
-            articleID_former = [max(article.id - 1, 0) for article in query_results]
-            articleID_after = [article.id + 1 for article in query_results]
-            retrievedArticles_former = chatbot_agent.client.retrieve(
-                collection_name="Articles", ids=articleID_former
-            )
-            retrievedArticles_after = chatbot_agent.client.retrieve(
-                collection_name="Articles", ids=articleID_after
-            )
-            requests = [
-                (
-                    chatbot_agent,
-                    # Concatenate the existing article content with the content retrieved using the article's id.
-                    # 'retrieve' function returns a list of points, so we need to access the first (and in this case, only) result with '[0]'.
-                    retrievedArticles_former[i].payload["content"]
-                    + "\n"
-                    + article.payload["content"]
-                    + "\n"
-                    + retrievedArticles_after[i].payload["content"],
-                    # Convert the chat history to string, including only user's side of the chat (user_only=True).
-                    chatbot_agent.convert_chat_history_to_string(user_only=True),
-                    "",
-                    query,
-                    article.payload["link"],
-                    article.score,
-                )
-                for i, article in enumerate(
-                    query_results
-                )  # Looping over each article in the query results.
-            ]
+    article_ids_plus_one = [
+        min(article.id + 1, 703 - 1) for article in query_results
+    ]
+    article_ids_minus_one = [max(article.id - 1, 0) for article in query_results]
+    retrieved_articles_plus_one = chatbot_agent.client.retrieve(
+        collection_name="Articles", ids=article_ids_plus_one
+    )
+    retrieved_articles_minus_one = chatbot_agent.client.retrieve(
+        collection_name="Articles", ids=article_ids_minus_one
+    )
+    requests = [
+        (
+            chatbot_agent,
+            # Concatenate the existing article content with the content retrieved using the article's id.
+            # 'retrieve' function returns a list of points, so we need to access the first (and in this case, only) result with '[0]'.
+            retrieved_articles_minus_one[i].payload["content"]
+            + "\n"
+            + article.payload["content"]
+            + "\n"
+            + retrieved_articles_plus_one[i].payload["content"],
+            chatbot_agent.convert_chat_history_to_string(user_only=True),
+            query,
+            article.payload["link"],
+            article.score,
+        )
+        for i, article in enumerate(query_results)
+    ]
 
-            # Use a Pool to manage the processes.
-            with ThreadPoolExecutor(max_workers=len(query_results)) as executor:
-                results = list(executor.map(process_request, requests))
+    # Use a Pool to manage the processes.
+    with ThreadPoolExecutor(max_workers=len(query_results)) as executor:
+        results = list(executor.map(process_request, requests))
 
-            # Initialize link_list_list with each link from link_list as a separate list.
-            link_list_list = [[link] for link in link_list]
-            # For each answer, perform the query and add the result to the corresponding list in link_list_list.
-            for i, answer in enumerate(answer_list):
-                secondary_query_results_temp = chatbot_agent.search_context_qdrant(
-                    answer, "Articles", top_k=2
-                )
-                link_list_list[i].extend(
-                    article.payload["link"]
-                    .replace("_sources", "")
-                    .replace(".md", ".html")
-                    for article in secondary_query_results_temp
-                )
+    # Results is a list of tuples of the form (answer, link).
+    answer_list, link_list = zip(*results)
 
-            combine_answer = chatbot_agent.prompt_combine_chain(
-                query=query, answer_list=answer_list, link_list_list=link_list_list
-            )
-            print(f"Query : {query}\n")
-            print(f"Answer: {combine_answer}\n")
-            chatbot_agent.update_chat_history(query, combine_answer)
-    else:
-        query = messages[-1]
-        answer_list = []
-        link_list = []
-        # query it using content vector.
-        query_results = chatbot_agent.search_context_qdrant(
-            chatbot_agent.convert_chat_history_to_string(
-                new_query=query, user_only=True
-            ),
-            "Articles",
-            top_k=4,
+    # Initialize link_list_list with each link from link_list as a separate list.
+    link_list_list = [[link] for link in link_list]
+    # For each answer, perform the query and add the result to the corresponding list in link_list_list.
+    for i, answer in enumerate(answer_list):
+        secondary_query_results_temp = chatbot_agent.search_context_qdrant(
+            answer, "Articles", top_k=2
+        )
+        link_list_list[i].extend(
+            article.payload["link"].replace("_sources", "").replace(".md", ".html")
+            for article in secondary_query_results_temp
         )
 
-        article_ids_plus_one = [
-            min(article.id + 1, 703 - 1) for article in query_results
-        ]
-        article_ids_minus_one = [max(article.id - 1, 0) for article in query_results]
-        retrieved_articles_plus_one = chatbot_agent.client.retrieve(
-            collection_name="Articles", ids=article_ids_plus_one
-        )
-        retrieved_articles_minus_one = chatbot_agent.client.retrieve(
-            collection_name="Articles", ids=article_ids_minus_one
-        )
-        requests = [
-            (
-                chatbot_agent,
-                # Concatenate the existing article content with the content retrieved using the article's id.
-                # 'retrieve' function returns a list of points, so we need to access the first (and in this case, only) result with '[0]'.
-                retrieved_articles_minus_one[i].payload["content"]
-                + "\n"
-                + article.payload["content"]
-                + "\n"
-                + retrieved_articles_plus_one[i].payload["content"],
-                chatbot_agent.convert_chat_history_to_string(user_only=True),
-                query,
-                article.payload["link"],
-                article.score,
-            )
-            for i, article in enumerate(query_results)
-        ]
+    combine_answer = chatbot_agent.prompt_combine_chain(
+        query=query, answer_list=answer_list, link_list_list=link_list_list
+    )
 
-        # Use a Pool to manage the processes.
-        with ThreadPoolExecutor(max_workers=len(query_results)) as executor:
-            results = list(executor.map(process_request, requests))
-
-        # Results is a list of tuples of the form (answer, link).
-        answer_list, link_list = zip(*results)
-
-        # Initialize link_list_list with each link from link_list as a separate list.
-        link_list_list = [[link] for link in link_list]
-        # For each answer, perform the query and add the result to the corresponding list in link_list_list.
-        for i, answer in enumerate(answer_list):
-            secondary_query_results_temp = chatbot_agent.search_context_qdrant(
-                answer, "Articles", top_k=2
-            )
-            link_list_list[i].extend(
-                article.payload["link"].replace("_sources", "").replace(".md", ".html")
-                for article in secondary_query_results_temp
-            )
-
-        combine_answer = chatbot_agent.prompt_combine_chain(
-            query=query, answer_list=answer_list, link_list_list=link_list_list
-        )
-
-        return combine_answer
+    return combine_answer
 
 
 if __name__ == "__main__":
